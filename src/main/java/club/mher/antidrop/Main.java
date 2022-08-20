e7e52ad00f3c3da41fe46605f605085e6d4edb3b
MherZaqaryan <mher.zaqaryann@gmail.com>
2022-08-20 17:23:54 +0400

    code clean up

--- src/main/java/club/mher/antidrop/Main.java
+++ src/main/java/club/mher/antidrop/Main.java
@@ -1,7 +1,8 @@
-package me.MrIronMan.AntiDrop;
+package club.mher.antidrop;
 
 import com.andrei1058.bedwars.api.BedWars;
 import com.andrei1058.bedwars.api.arena.GameState;
+import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -21,29 +22,30 @@ public class Main extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        loadBedWars1058();
-        Bukkit.getPluginManager().registerEvents(this, this);
-    }
-
-    @EventHandler
-    public void onItemDrop(PlayerDropItemEvent e) {
-        if (e.isCancelled()) return;
-        Player player = e.getPlayer();
-        if (!bedwarsAPI.getArenaUtil().isPlaying(player)) return;
-        if (!bedwarsAPI.getArenaUtil().getArenaByPlayer(player).getStatus().equals(GameState.playing)) return;
-        Block block = player.getLocation().getBlock().getRelative(BlockFace.DOWN);
-        if (!block.getType().equals(Material.AIR)) return;
-        e.setCancelled(true);
-    }
-
-    public void loadBedWars1058() {
         if (!Bukkit.getPluginManager().isPluginEnabled("BedWars1058")) {
-            getLogger().severe("BedWars1058 was not found. Disabling addon...");
+            getLogger().severe("BedWars1058 was not found, disabling...");
             Bukkit.getPluginManager().disablePlugin(this);
             return;
         }
-        this.metrics = new Metrics(this, bStatsId);
         bedwarsAPI = Bukkit.getServicesManager().getRegistration(BedWars.class).getProvider();
+        Bukkit.getPluginManager().registerEvents(this, this);
+        this.metrics = new Metrics(this, bStatsId);
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onItemDrop(PlayerDropItemEvent e) {
+        Player player = e.getPlayer();
+        if (!bedwarsAPI.getArenaUtil().isPlaying(player)) {
+            return;
+        }
+        if (!bedwarsAPI.getArenaUtil().getArenaByPlayer(player).getStatus().equals(GameState.playing)) {
+            return;
+        }
+        Block block = player.getLocation().getBlock().getRelative(BlockFace.DOWN);
+        if (!block.getType().equals(Material.AIR)) {
+            return;
+        }
+        e.setCancelled(true);
     }
 
 }
